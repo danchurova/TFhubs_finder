e99de113d9671abe458cb2df74249dbb5633a535
danchurova <31286513+danchurova@users.noreply.github.com>
2017-11-13 16:32:36 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+1. You need to have in the same directory pipeline.sh, intersect.py and enhancers.py.2. In the same directory you should have another directory there will be .narrowPeak files and gencode.v19.annotation.gff3.
+
---
+++ enhancers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def main():
+    promoter_peaks = set()
+    promoters = open(sys.argv[1])
+    all_peaks = open(sys.argv[2])
+
+    for line in promoters.readlines():
+        peak_1 = line.strip()
+        promoter_peaks.add(peak_1)
+
+    for line_2 in all_peaks.readlines():
+        pieces = line_2.strip().split("\t")
+        chrom, start, end, peak_2,_,_,_,_,_,_ = pieces
+        if peak_2 not in promoter_peaks:
+            print(chrom, start, end, peak_2)
+        
+if __name__ == '__main__':
+    main()	
+	
